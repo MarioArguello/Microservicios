7e04441b569617fb6b5ecbffd27ec7a1cdaf9df2
Mario Arguello <60244917+MarioArguello@users.noreply.github.com>
2023-02-16 20:07:21 -0500

    relacion entre cliente y muchos productos

--- businessdomain/customer/src/main/java/com/gordis/customer/entities/CustomerProduct.java
+++ businessdomain/customer/src/main/java/com/gordis/customer/entities/CustomerProduct.java
@@ -4,10 +4,31 @@
  */
 package com.gordis.customer.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Transient;
+import lombok.Data;
+
 /**
  *
  * @author argue
  */
+@Data
 class CustomerProduct {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private long id;
+    private long productId;
+    @Transient
+    private long productName;
     
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY, targetEntity = Customer.class)
+    @JoinColumn(name = "customerId", nullable = true)
+    private Customer customer;
 }
